FrancescoFerraro/imx-atf

commit 32f8832e8c7360510061c90318742b781f58a1f6

MA-19048-2 MCU security enhancement
Mapping the TCM memory in ATF so HAB verify can pass.
Restrict the MCU (Domain 1) write access to RDC and setup
the MCU RDC config when MCU starts/stops.

Change-Id: I1a90598095171abc9f57d305907b25ce66441cf3
Signed-off-by: Ji Luo <ji.luo@nxp.com>
Reviewed-by: Ye Li <ye.li@nxp.com>

plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -34,7 +34,7 @@
 static const mmap_region_t imx_mmap[] = {
 	GIC_MAP, AIPS_MAP, OCRAM_S_MAP, DDRC_MAP,
 	NOC_MAP, CAAM_RAM_MAP, NS_OCRAM_MAP,
-	ROM_MAP, DRAM_MAP, VPU_BLK_CTL_MAP, {0},
+	ROM_MAP, DRAM_MAP, VPU_BLK_CTL_MAP, TCM_MAP, {0},
 };
 
 static const struct aipstz_cfg aipstz[] = {
@@ -52,6 +52,7 @@ static const struct imx_rdc_cfg rdc[] = {
 	/* peripherals domain permission */
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
 	RDC_PDAPn(RDC_PDAP_WDOG1, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
 
 	/* memory region */
 

plat/imx/imx8m/gpc_common.c
@@ -19,6 +19,7 @@
 #include <imx8m_psci.h>
 #include <imx_sip_svc.h>
 #include <plat_imx8.h>
+#include <imx_rdc.h>
 
 static uint32_t gpc_imr_offset[] = { IMR1_CORE0_A53, IMR1_CORE1_A53, IMR1_CORE2_A53, IMR1_CORE3_A53, };
 
@@ -336,6 +337,10 @@ int imx_gpc_handler(uint32_t smc_fid, u_register_t x1, u_register_t x2, u_regist
 	return 0;
 }
 
+#if defined(PLAT_imx8mn) || defined(PLAT_imx8mp)
+#define MCU_RDC_MAGIC "mcu_rdc"
+#endif
+
 #pragma weak imx_src_handler
 /* imx8mq/imx8mm need to verrride below function */
 int imx_src_handler(uint32_t smc_fid, u_register_t x1, u_register_t x2,
@@ -345,9 +350,17 @@ int imx_src_handler(uint32_t smc_fid, u_register_t x1, u_register_t x2,
 	uint64_t timeout;
 	int ret1 = 0, ret2 = 0;
 	uint32_t offset;
+#if defined(PLAT_imx8mn) || defined(PLAT_imx8mp)
+	uint64_t len = (strlen(MCU_RDC_MAGIC) + 3) & ~(3);
+#endif
 
 	switch(x1) {
 	case IMX_SIP_SRC_M4_START:
+		/* Setup RDC config for MCU */
+#if defined(PLAT_imx8mn) || defined(PLAT_imx8mp)
+		if (!memcmp((void *)IMX8M_MCU_RDC_START_CONFIG_ADDR, MCU_RDC_MAGIC, strlen(MCU_RDC_MAGIC)))
+			imx_rdc_init((struct imx_rdc_cfg *)(IMX8M_MCU_RDC_START_CONFIG_ADDR + len));
+#endif
 		mmio_clrbits_32(IMX_IOMUX_GPR_BASE + 0x58, 0x1);
 		break;
 	case IMX_SIP_SRC_M4_STARTED:
@@ -369,6 +382,12 @@ int imx_src_handler(uint32_t smc_fid, u_register_t x1, u_register_t x2,
 		 * h)	Set GPR.INITVTOR
 		 * i)	Set GPR.CPUWAIT=0,  M7 starting running
 		 */
+		/* Restore rdc config for mcu */
+#if defined(PLAT_imx8mn) || defined(PLAT_imx8mp)
+		if (!memcmp((void *)IMX8M_MCU_RDC_START_CONFIG_ADDR, MCU_RDC_MAGIC, strlen(MCU_RDC_MAGIC)))
+			imx_rdc_init((struct imx_rdc_cfg *)(IMX8M_MCU_RDC_STOP_CONFIG_ADDR + len));
+#endif
+
 		offset = LPS_CPU1;
 		val = mmio_read_32(IMX_GPC_BASE + offset);
 		/* Not in stop/wait mode */

plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
@@ -33,7 +33,7 @@
 
 static const mmap_region_t imx_mmap[] = {
 	GIC_MAP, AIPS_MAP, OCRAM_S_MAP, DDRC_MAP,
-	CAAM_RAM_MAP, NS_OCRAM_MAP, ROM_MAP, DRAM_MAP,
+	CAAM_RAM_MAP, NS_OCRAM_MAP, ROM_MAP, DRAM_MAP, TCM_MAP,
 	{0},
 };
 
@@ -52,6 +52,7 @@ static const struct imx_rdc_cfg rdc[] = {
 	/* peripherals domain permission */
 	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
 	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
 
 	/* memory region */
 	RDC_MEM_REGIONn(16, 0x0, 0x0, 0xff),

plat/imx/imx8m/imx8mn/include/platform_def.h
@@ -96,6 +96,10 @@
 #define IMX_CAAM_RAM_SIZE		U(0x10000)
 #define IMX_DRAM_BASE			U(0x40000000)
 #define IMX_DRAM_SIZE			U(0xc0000000)
+#define IMX_TCM_BASE			U(0x7E0000)
+#define IMX_TCM_SIZE			U(0x40000)
+#define IMX8M_MCU_RDC_START_CONFIG_ADDR U(0x186000)
+#define IMX8M_MCU_RDC_STOP_CONFIG_ADDR  U(0x187000)
 
 #define IMX_GIC_BASE			PLAT_GICD_BASE
 #define IMX_GIC_SIZE			U(0x200000)
@@ -160,5 +164,6 @@
 #define NS_OCRAM_MAP	MAP_REGION_FLAT(IMX_NS_OCRAM_BASE, IMX_NS_OCRAM_SIZE, MT_MEMORY | MT_RW) /* NS OCRAM */
 #define ROM_MAP		MAP_REGION_FLAT(IMX_ROM_BASE, IMX_ROM_SIZE, MT_MEMORY | MT_RO) /* ROM code */
 #define DRAM_MAP	MAP_REGION_FLAT(IMX_DRAM_BASE, IMX_DRAM_SIZE, MT_MEMORY | MT_RW | MT_NS) /* DRAM */
+#define TCM_MAP		MAP_REGION_FLAT(IMX_TCM_BASE, IMX_TCM_SIZE, MT_MEMORY | MT_RW | MT_NS) /* TCM */
 
 #endif /* platform_def.h */

plat/imx/imx8m/imx8mp/include/platform_def.h
@@ -98,6 +98,10 @@
 #define IMX_DRAM_SIZE			U(0xc0000000)
 #define IMX_VPU_BLK_BASE		U(0x38330000)
 #define IMX_VPU_BLK_SIZE		U(0x10000)
+#define IMX_TCM_BASE			U(0x7E0000)
+#define IMX_TCM_SIZE			U(0x40000)
+#define IMX8M_MCU_RDC_START_CONFIG_ADDR U(0x186000)
+#define IMX8M_MCU_RDC_STOP_CONFIG_ADDR  U(0x187000)
 
 #define IMX_GIC_BASE			PLAT_GICD_BASE
 #define IMX_GIC_SIZE			U(0x200000)
@@ -174,6 +178,7 @@
 #define ROM_MAP		MAP_REGION_FLAT(IMX_ROM_BASE, IMX_ROM_SIZE, MT_MEMORY | MT_RO) /* ROM code */
 #define DRAM_MAP	MAP_REGION_FLAT(IMX_DRAM_BASE, IMX_DRAM_SIZE, MT_MEMORY | MT_RW | MT_NS) /* DRAM */
 #define VPU_BLK_CTL_MAP	MAP_REGION_FLAT(IMX_VPU_BLK_BASE, IMX_VPU_BLK_SIZE, MT_DEVICE | MT_RW) /* VPU BLK CTL */
+#define TCM_MAP		MAP_REGION_FLAT(IMX_TCM_BASE, IMX_TCM_SIZE, MT_MEMORY | MT_RW | MT_NS) /* TCM */
 
 
 #endif /* platform_def.h */

commit bfec89a61f84cc77a6082b006e03c390005a0550

plat: imx8mp: Add D1W permission to fix M7 start failure
Without this patch the M7 doesen't start causing kernel boot failure
The issue is introduced by commit 32f8832e8c7360510061c90318742b781f58a1f6

Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -51,7 +51,7 @@ static const struct imx_rdc_cfg rdc[] = {
 
 	/* peripherals domain permission */
 	RDC_PDAPn(RDC_PDAP_WDOG1, D0R | D0W),
-	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
+	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R | D1W),
 
 	/* memory region */
 